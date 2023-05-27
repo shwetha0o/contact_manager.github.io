64563232e9458dd744feef2b3854355e7af5bedc
shwetha manaswini <shwethamanaswini1369@gmail.com>
2023-05-27 23:08:54 +0530

    files uploaded

---
+++ contact-book/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./components/App";
+
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// reportWebVitals();
